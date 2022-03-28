82021d184a1099d62ef5266af72fee2314edf57d
wang <jcvaldez199@gmail.com>
2022-03-28 10:42:49 +0800

    made faster pushdata

--- rdbms_scripts/pushdata.py
+++ rdbms_scripts/pushdata.py
@@ -9,18 +9,19 @@ import psycopg2
 if __name__ == '__main__':
     frame_max = 11
 
-
-
     with open("datafile.json") as f:
         data  = json.load(f)
 
         conn = psycopg2.connect(dbname="gpstest")
         curs = conn.cursor()
+
+        query_string = 'INSERT INTO picture(coord, frame, grayscale) VALUES '
+        for x in data:
+            query_string = query_string+'(ST_MakePoint('+str(x["loc"][0])+','+str(x["loc"][1])+'), frm, '+str(x["grayscale"])+'),'
+
         for cnt in range(1, frame_max):
-            query_string = 'INSERT INTO picture(coord, frame, grayscale) VALUES '
-            for x in data:
-                query_string = query_string+'(ST_MakePoint('+str(x["loc"][0])+','+str(x["loc"][1])+'), '+str(cnt)+', '+str(x["grayscale"])+'),'
-            curs.execute(query_string[:-1])
+            commit_string = query_string.replace('frm',str(cnt))
+            curs.execute(commit_string[:-1])
             print(cnt)
 
         conn.commit()
